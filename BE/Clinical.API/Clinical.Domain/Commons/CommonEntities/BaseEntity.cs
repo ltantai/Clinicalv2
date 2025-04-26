741e4b808376617ea57c59338cef9b8b5913750a
TaiLe <lttai64e1@gmail.com>
2025-04-26 13:03:51 +0700

    #TL-"Refactor UI and add audit info"

--- BE/Clinical.API/Clinical.Domain/Commons/CommonEntities/BaseEntity.cs
+++ BE/Clinical.API/Clinical.Domain/Commons/CommonEntities/BaseEntity.cs
@@ -13,5 +13,6 @@ namespace Clinical.Domain.Commons.CommonEntities
         public int? ModifyBy { get; set; }
         public DateTime? CreateTime { get; set; }
         public int? CreateBy { get; set; }
+        public bool IsDelete { get; set; }
     }
 }
