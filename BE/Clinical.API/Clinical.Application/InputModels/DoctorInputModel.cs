bef58a0370ae1dc01b21e02bf8920bc0f1711043
TaiLe <lttai64e1@gmail.com>
2025-05-06 22:53:09 +0700

    #TL-"Implement doctor APIs on the BE side"

---
+++ BE/Clinical.API/Clinical.Application/InputModels/DoctorInputModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinical.Application.InputModels
+{
+    public class DoctorInputModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ BE/Clinical.API/Clinical.Application/Interfaces/IDoctorService.cs
@@ -0,0 +1,23 @@
+﻿using Clinical.Application.InputModels;
+using Clinical.Application.InputModels.PaginationModels;
+using Clinical.Application.ViewModels;
+using Clinical.Domain.Entitites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinical.Application.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<DoctorViewModel> GetDoctorById(int id);
+        Task<List<DoctorViewModel>> GetAll();
+        Task<PaginatedResponse<DoctorViewModel>> GetPaginatedDoctorsAsync(string search, int pageNumber, int pageSize);
+        Task Add(DoctorInputModel doctor);
+        Task Update(DoctorInputModel doctor);
+        Task Delete(int doctorId);
+    }
+}
